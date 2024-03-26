f9df6ba5f9ef9b8774f1441957b753b4a552aa64
Raphael Darras <raphael.darrras@gmail.com>
2024-03-26 21:34:43 +0100

    Happy path for login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login spec', () => {
+  it('Log with standard user', () => {
+    cy.visit('https://www.saucedemo.com/')
+
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+
+    cy.get('#header_container')
+    cy.get('#inventory_container')
   })
 })
