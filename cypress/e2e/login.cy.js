83a324a8900f4a5775330fdbbef3b7325548cc1b
Raphael Darras <raphael.darrras@gmail.com>
2024-03-26 22:31:10 +0100

    Add login with locked out user

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,5 @@
 describe('Login spec', () => {
+
   it('Log with standard user', () => {
     cy.visit('https://www.saucedemo.com/')
 
@@ -9,4 +10,17 @@ describe('Login spec', () => {
     cy.get('#header_container')
     cy.get('#inventory_container')
   })
+
+  it('Log with locked out user', () => {
+    cy.visit('https://www.saucedemo.com/')
+
+    cy.get('#user-name').type('locked_out_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('.error-message-container').should('not.have.class','error')
+    cy.get('#login-button').click()
+
+    cy.get('.error-message-container').should('have.class','error')
+    cy.get('[data-test=error]').should('have.text','Epic sadface: Sorry, this user has been locked out.')
+  })
+
 })
