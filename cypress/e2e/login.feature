52a96b9cc8a73f7300e8cc3ed705952c0829cddc
Raphael Darras <raphael.darrras@gmail.com>
2024-03-27 21:59:05 +0100

    Introduce gherkin parameters

--- cypress/e2e/login.feature
+++ cypress/e2e/login.feature
@@ -1,10 +1,10 @@
 Feature: Login
 	Scenario: Log as standard user
 		Given I am on the login page
-		When I enter the standard user credentials
+		When I enter the 'standard' user credentials
 		Then I am on the inventory page
 
 	Scenario: Log as locked out user
 		Given I am on the login page
-		When I enter the locked out user credentials
+		When I enter the 'locked out' user credentials
 		Then I see an error message on the login page
--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -4,18 +4,16 @@ Given("I am on the login page", () => {
   cy.visit('/')
 })
 
-When("I enter the standard user credentials", () => {
-  cy.login('standard_user', 'secret_sauce')
+When("I enter the {string} user credentials", (user) => {
+  cy.fixture('credentials').then(credentials => {
+    cy.login(credentials[user], 'secret_sauce')
+  })
 })
 
 Then("I am on the inventory page", () => {
   cy.url().should('include', 'inventory.html')
   cy.get('#header_container')
   cy.get('#inventory_container')
-})
-
-When("I enter the locked out user credentials", () => {
-  cy.login('locked_out_user', 'secret_sauce')
 })
 
 Then("I see an error message on the login page", () => {
