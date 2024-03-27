20511ec71e2826ab427a76367f92c2564760c3ec
Raphael Darras <raphael.darrras@gmail.com>
2024-03-27 19:03:36 +0100

    Add login custom command

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,9 +3,7 @@ describe('Login spec', () => {
   it('Log with standard user', () => {
     cy.visit('/')
 
-    cy.get('#user-name').type('standard_user')
-    cy.get('#password').type('secret_sauce')
-    cy.get('#login-button').click()
+    cy.login('standard_user', 'secret_sauce')
 
     cy.get('#header_container')
     cy.get('#inventory_container')
@@ -14,10 +12,9 @@ describe('Login spec', () => {
   it('Log with locked out user', () => {
     cy.visit('/')
 
-    cy.get('#user-name').type('locked_out_user')
-    cy.get('#password').type('secret_sauce')
     cy.get('.error-message-container').should('not.have.class','error')
-    cy.get('#login-button').click()
+
+    cy.login('locked_out_user', 'secret_sauce')
 
     cy.get('.error-message-container').should('have.class','error')
     cy.get('[data-test=error]').should('have.text','Epic sadface: Sorry, this user has been locked out.')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (email, password) => {
+    cy.get('#user-name').type(email)
+    cy.get('#password').type(password)
+    cy.get('#login-button').click()
+})
