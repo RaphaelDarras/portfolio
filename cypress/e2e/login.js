4d09a47cbd57d252bc9857970b3076d6f6f7e789
Raphael Darras <raphael.darrras@gmail.com>
2024-03-27 21:15:57 +0100

    introduce cucumber

---
+++ cypress/e2e/login.js
@@ -0,0 +1,46 @@
+const {Given, When, Then } = require("@badeball/cypress-cucumber-preprocessor");
+
+Given("I am on the login page", () => {
+  cy.visit('/')
+})
+
+When("I enter the standard user credentials", () => {
+  cy.login('standard_user', 'secret_sauce')
+})
+
+Then("I am on the inventory page", () => {
+  cy.url().should('include', 'inventory.html')
+  cy.get('#header_container')
+  cy.get('#inventory_container')
+})
+
+When("I enter the locked out user credentials", () => {
+  cy.login('locked_out_user', 'secret_sauce')
+})
+
+Then("I see an error message on the login page", () => {
+  cy.url().should('not.include', 'inventory.html')
+  cy.get('.error-message-container').should('have.class','error')
+  cy.get('[data-test=error]').should('have.text','Epic sadface: Sorry, this user has been locked out.')
+})
+
+
+
+// describe('Login spec', () => {
+  
+//   beforeEach(() => {
+//   })
+
+//   it('Log with standard user', () => {
+//   })
+
+//   it('Log with locked out user', () => {
+//     cy.get('.error-message-container').should('not.have.class','error')
+
+//     cy.login('locked_out_user', 'secret_sauce')
+
+//     cy.get('.error-message-container').should('have.class','error')
+//     cy.get('[data-test=error]').should('have.text','Epic sadface: Sorry, this user has been locked out.')
+//   })
+
+// })
