605a1877f19741fee6c0926cdd881b3515c62f5c
Raphael Darras <raphael.darrras@gmail.com>
2024-03-31 09:57:52 +0200

    Improve increase/decrease check

--- cypress/e2e/inventory.js
+++ cypress/e2e/inventory.js
@@ -22,16 +22,29 @@ Then("every inventory item is present and complete", () => {
 })
 
 When("I click on the {string} button of the {string}", (action, item) => {
+  cy.get('body')
+    .then(($body) => {
+      if($body.find('[data-test=shopping-cart-badge]').length) {
+        cy.get('[data-test=shopping-cart-badge]')
+          .invoke('text')
+          .then(cartBadgeText => {
+            this.initialCartTotal = parseInt(cartBadgeText, 10)
+          })
+      } else {
+        this.initialCartTotal = 0
+      }
+    })
+
   if(action === 'Add to cart') {
     cy.get('[data-test=shopping-cart-badge]')
       .should('not.exist')
 
     cy.addToCart(item)
-    
+        
     cy.get('[data-test=shopping-cart-badge]')
     .invoke('text')
-    .then((cartBadgeValue) => {
-      this.cartTotal = cartBadgeValue
+    .then((cartBadgeText) => {
+      this.newCartTotal = parseInt(cartBadgeText, 10)
     })
   } else {
     cy.addToCart(item)
@@ -44,8 +57,12 @@ When("I click on the {string} button of the {string}", (action, item) => {
 })
 
 Then("the cart total has been {string}", (variation) => {
-  variation === 'increased' ?
-    expect(this.cartTotal).to.equal('1') :
+  if(variation === 'increased') {
+    expect(this.newCartTotal).to.equal(this.initialCartTotal+1)
+  } else if( variation === 'decreased' && this.initialCartTotal != 0) {
+    expect(this.newCartTotal).to.equal(this.initialCartTotal-1)
+  } else {
     cy.get('[data-test=shopping-cart-badge]')
       .should('not.exist')
+  }
 })
