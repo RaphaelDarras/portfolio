a0d0a2de081be7caf1d75ee6054a8f03416c9145
Raphael Darras <raphael.darrras@gmail.com>
2024-04-02 12:27:30 +0200

    Fix create call

--- api/pet.spec.mjs
+++ api/pet.spec.mjs
@@ -1,25 +1,21 @@
 import request from 'supertest'
 import { expect } from 'chai'
-import * as pet from './data/pet.json'assert { type: 'json'}
+import * as pet from './data/pet.json' assert { type: 'json'}
 
 describe('Test petstore\s pets endpoints', () => {
     const baseUrl = 'https://petstore.swagger.io/'
-    let fuzzId = ''
-    let existingPet = ''
+    let fuzzPet = ''
 
     it('should create good doggo Fuzz', (done) => {
-        // This test return 200 and an Id but for a reason I ignore I can't find
-        // this Id later so I'm not sure of the status of this test
         request(baseUrl)
             .post('v2/pet')
-            .send(pet)
+            .send(pet.default)
             .set('Accept', 'application/json')
             .set('content-type', 'application/json')
             .end(function(err, response) {
                 expect(response.statusCode).to.be.equal(200)
                 // storing created pet Id here to reuse later
-                // But it does not work
-                fuzzId = response.body.id
+                fuzzPet = response.body
                 if(err) {
                     throw err
                 }
@@ -31,13 +27,10 @@ describe('Test petstore\s pets endpoints', () => {
         request(baseUrl)
             .get('v2/pet/findByStatus?status=available')
             .set('Accept', 'application/json')
-            .set('content-type', 'application/json')
             .end(function(err, response) {
                 //Checks to do
                 expect(response.statusCode).to.be.equal(200)
                 expect(response.body.length).to.be.greaterThan(0)
-                // Storing an existing Id to reuse later
-                existingPet = response.body[1]
                 if(err) {
                     throw err
                 }
@@ -46,19 +39,15 @@ describe('Test petstore\s pets endpoints', () => {
     })
 
     it('should find existing pet by Id', (done) => {
-        // For the test to work, I'll use the Id of the first pet
-        // returned by findByStatus method
         request(baseUrl)
-            .get('v2/pet/'+existingPet.id)
+            .get('v2/pet/'+fuzzPet.id)
             .set('Accept', 'application/json')
-            .set('content-type', 'application/json')
             .end(function(err, response) {
                 expect(response.statusCode).to.be.equal(200)
-
-                // expect(response.body).to.be.equal(existingPet)
-                // I'll only compare Ids instead of the whole pet object 
-                // Because for the same Id, api return different objects
-                expect(response.body.id).to.be.equal(existingPet.id)
+                expect(response.body.name).to.be.equal(fuzzPet.name)
+                expect(response.body.category.name).to.be.equal(fuzzPet.category.name)
+                expect(response.body.tags.name).to.be.equal(fuzzPet.tags.name)
+                expect(response.body.status).to.be.equal(fuzzPet.status)
                 if(err) {
                     throw err
                 }
